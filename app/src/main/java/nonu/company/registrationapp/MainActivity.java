4675e7be06dbaa39821b1700d42a5be39255dd3e
nonuabi <nonuabi@gmail.com>
2020-11-01 12:38:43 +0530

    Update design part and added  one text view

--- app/src/main/java/nonu/company/registrationapp/MainActivity.java
+++ app/src/main/java/nonu/company/registrationapp/MainActivity.java
@@ -3,12 +3,21 @@ package nonu.company.registrationapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_mail_2);
+    }
+    public void onClickButton(View view) {
+        TextView textView = findViewById(R.id.tvname);
+        EditText editTextName = findViewById(R.id.etname);
+        textView.setText("Name : "+ editTextName.getText().toString());
+
     }
 }

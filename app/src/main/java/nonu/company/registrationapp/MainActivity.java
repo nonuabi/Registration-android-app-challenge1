c3fcf4ab3b80710ca19de238a5d1bbcefc6684a2
nonuabi <nonuabi@gmail.com>
2020-11-01 12:46:12 +0530

    java part done and also app is ready

--- app/src/main/java/nonu/company/registrationapp/MainActivity.java
+++ app/src/main/java/nonu/company/registrationapp/MainActivity.java
@@ -18,6 +18,13 @@ public class MainActivity extends AppCompatActivity {
         TextView textView = findViewById(R.id.tvname);
         EditText editTextName = findViewById(R.id.etname);
         textView.setText("Name : "+ editTextName.getText().toString());
+        TextView textViewLast = findViewById(R.id.tvlast);
+        EditText editTextLast = findViewById(R.id.etlast);
+        textViewLast.setText("Last : "+ editTextLast.getText().toString());
+        TextView textViewEmail = findViewById(R.id.tvemail);
+        EditText editTextEmail = findViewById(R.id.etemail);
+        textViewEmail.setText("Email : "+ editTextEmail.getText().toString());
+
 
     }
 }
